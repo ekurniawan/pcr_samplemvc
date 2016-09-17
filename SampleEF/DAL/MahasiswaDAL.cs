a5d0bc3a15a889817cd4e5acc05dda665c7643b5
erick <erick@DESKTOP-7DP9Q9N>
2016-09-17 09:29:25 +0700

    mengimplementasikan interface IMasterClass pada class MahasiswaDAL

--- SampleEF/DAL/MahasiswaDAL.cs
+++ SampleEF/DAL/MahasiswaDAL.cs
@@ -3,9 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using SampleEF.Models;
+
 namespace SampleEF.DAL
 {
-    public class MahasiswaDAL
+    public class MahasiswaDAL : IMasterClass<Mahasiswa>
     {
+        public void Delete(Mahasiswa obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Mahasiswa> GetAll(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Mahasiswa GetById(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Mahasiswa obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Mahasiswa obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
