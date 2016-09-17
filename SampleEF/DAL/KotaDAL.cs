71374babd54938a03ad5abf80fdfd8dd88ea609c
erick <erick@DESKTOP-7DP9Q9N>
2016-09-17 09:06:19 +0700

    mengimplementasikan interface IMasterClass

--- SampleEF/DAL/KotaDAL.cs
+++ SampleEF/DAL/KotaDAL.cs
@@ -3,9 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using SampleEF.Models;
+
 namespace SampleEF.DAL
 {
-    public class KotaDAL
+    public class KotaDAL : IMasterClass<Kota>
     {
+        public void Delete(Kota obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Kota> GetAll(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Kota GetById(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Kota obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Kota obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
